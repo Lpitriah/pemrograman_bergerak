80b16d4057981000132371748a5a448e31daabd2
Lpitriah <lpitriah12d@gmail.com>
2026-02-19 12:32:50 +0700

    p9

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,80 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonButton,
+  IonInput,
+  IonCard,
+  IonCardContent
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonButton,
+    IonInput,
+    IonCard,
+    IonCardContent,
+    CommonModule,
+    FormsModule
+  ],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+
+  listBarang: any[] = [];
+  namaBaru = '';
+  hargaBaru = '';
+
+  constructor(private api: ApiService) {}
+
+  ngOnInit() {
+    this.ambilData();
+  }
+
+  ambilData() {
+    this.api.getBarang().subscribe({
+      next: (res) => this.listBarang = res,
+      error: (err) => console.error('Error:', err)
+    });
+  }
+
+  kirimData() {
+    if (!this.namaBaru || !this.hargaBaru) return;
+
+    const data = {
+      nama: this.namaBaru,
+      harga: this.hargaBaru
+    };
+
+    this.api.tambahBarang(data).subscribe({
+      next: () => {
+        alert('Data Berhasil Dikirim!');
+        this.namaBaru = '';
+        this.hargaBaru = '';
+        this.ambilData();
+      },
+      error: (err) => {
+        alert('Gagal kirim data. Cek Console.');
+        console.error(err);
+      }
+    });
+  }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { RouteReuseStrategy, provideRouter} from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
+// 1. Import HttpClient
+import { provideHttpClient } from '@angular/common/http';
+
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes),
+
+    // 2. Daftarkan Provider
+    provideHttpClient(),
   ],
 });
